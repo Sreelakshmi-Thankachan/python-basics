2e70eef9b4a3149e43f55aab029ef4c8adf05122
Sreelakshmi Thankachan <thadathilsreelakshmi@gmail.com>
2021-11-13 22:46:48 +0530

    Sieve login implemented

--- SieveOfEratostenes.py
+++ SieveOfEratostenes.py
@@ -1,7 +1,24 @@
 '''
 Simple demonstration of Sieve's algorithm
 '''
-def  SieveOfEratosthenes(n):
-    pass
 
 
+def SieveOfEratosthenes(n):
+    prime = [True for i in range(2, n+1)]
+    p = 2
+
+    while(p * p < n):
+        if prime[p]:
+            for i in range(p*p, (n+1), p):  # multiple of p that are >= p*p
+                prime[i] = False
+
+        p += 1
+
+    for p in range(2, n+1):
+        if prime[p]:
+            print(p, end=', ')
+
+
+n = int(input('Please enter the number: '))
+
+SieveOfEratosthenes(n)
